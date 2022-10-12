ee500b82d5a4c61d0f70f9be3daacf6b8cee9e7d
Vítězslav Kříž <sprtokiller.6c@gmail.com>
2022-10-12 19:08:04 +0200

    Cell properties

--- include/Cell.h
+++ include/Cell.h
@@ -7,13 +7,20 @@
  */
 
 #pragma once
+#include <vector>
 
 class Cell
 {
 public:
 	Cell();
 	~Cell();
-
+	inline size_t getW() const { return W; }
+	inline size_t getI() const { return I; }
+	inline size_t getB() const { return B; }
+	inline size_t getC() const { return C; }
 private:
-
+	size_t W; //water particles
+	size_t I; //ink particles
+	size_t B; //height of bottom (water capacity)
+	size_t C; //capacity of water captured to cells
 };
