832308724ca82c2c8eebf2205d9af01e1eb2c927
Vítězslav Kříž <sprtokiller.6c@gmail.com>
2022-10-13 18:59:43 +0200

    Better constants

--- include/MagicConstants.h
+++ include/MagicConstants.h
@@ -8,7 +8,20 @@
 
 #pragma once
 
-const size_t DEFAULT_B = 100;
-const size_t DEFAULT_C = 100;
-const size_t FIBERS_PER_CELL = 1;
-const size_t FIBER_LEN = 8;
+
+#define um *1
+#define mm *1000
+#define cm *1000*10
+#define m  *1000*1000
+
+constexpr size_t CELL_SIZE = 20 um;
+constexpr size_t FIBER_LEN = 3 cm / CELL_SIZE;
+constexpr size_t DEFAULT_B = 100 um;
+constexpr size_t DEFAULT_C = 100 um;
+// for every 50 cells, generate a random fiber
+constexpr size_t C_PER_FIBER = 50; 
+
+#undef um
+#undef mm
+#undef cm
+#undef m
