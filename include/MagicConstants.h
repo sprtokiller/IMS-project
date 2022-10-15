941ecb5f05079d8e8fd3b09385c068001097ed1d
KoMatrich <kocichmartin@gmail.com>
2022-10-15 11:40:55 +0200

    Dimension calculation automated

--- include/MagicConstants.h
+++ include/MagicConstants.h
@@ -10,9 +10,9 @@
 #define MAGICCONSTANTS_H
 
 #define um *1
-#define mm *1000
-#define cm *1000*10
-#define m  *1000*1000
+#define mm um*1000
+#define cm mm*10
+#define m  cm*1000
 
 struct Paper {
 	uint FIBER_LEN;
@@ -27,8 +27,8 @@ constexpr Paper SBHK = { 15 mm / CELL_SIZE, 500 };
 constexpr uint DEFAULT_B = 10 mm; //100 um;
 constexpr uint DEFAULT_C = 10 mm; //100 um;
 
-constexpr size_t DEFAULT_WIDTH = 5 cm / CELL_SIZE;
-constexpr size_t DEFAULT_HEIGHT = 5 cm / CELL_SIZE;
+constexpr size_t DEFAULT_WIDTH = 1 cm / CELL_SIZE;
+constexpr size_t DEFAULT_HEIGHT = 1 cm / CELL_SIZE;
 
 #undef um
 #undef mm
